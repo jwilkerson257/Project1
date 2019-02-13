a7c492ba2441b7fe97633d31406161088eaa5c20
Josh <Jwilkerson@ou.edu>
2019-02-12 19:46:42 -0600

    Initialized variables and created constructor

---
+++ HammingDist.java
@@ -0,0 +1,19 @@
+public class HammingDist
+{
+	String strg1;
+	String strg2;
+	int idk = 0;
+	int node1;
+	int node2;
+	int node3;
+	int node4;
+	
+	
+	public HammingDist(String strg1, String strg2)
+	{
+		this.strg1 = strg1;
+		this.strg2 = strg2;
+	}
+	
+	
+}
